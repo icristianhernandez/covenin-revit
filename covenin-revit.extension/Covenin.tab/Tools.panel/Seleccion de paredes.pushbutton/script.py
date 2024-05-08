c2776ec6f17bf66a98e9cd2e6a0935136c1aabee
gusalviarez <gusalv2001@gmail.com>
2024-05-07 22:40:56 -0400

    add h,v,a

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -6,45 +6,35 @@ uidoc = __revit__.ActiveUIDocument
 
 walls = []
 
-def get_wall_length(document, wall):
-
-  # Check if the element is a valid wall
-  if not isinstance(wall, Wall):
-    return None
-
-  # Get the wall's location curve
-  location_curve = wall.Location
-
-  # Access the curve object from the location curve
-  curve = location_curve.Curve
-
-  # Get the start and end points of the curve
-  start_point = curve.GetEndPoint(0)
-  end_point = curve.GetEndPoint(1)
-
-  # Calculate the distance between the points using XYZ.DistanceTo
-  length = start_point.DistanceTo(end_point)
-
-  return length
-
 class SelectionFilter_Walls(ISelectionFilter):
     def AllowElement(self, element):
         if type(element) == Wall:
             return True
-
 
 Wall_filter = SelectionFilter_Walls()
 try:
     ref_picked_walls = uidoc.Selection.PickObjects(ObjectType.Element, Wall_filter)
     picked_walls = [doc.GetElement(ref) for ref in ref_picked_walls]
     for wall in picked_walls:
+        width = wall.Width * 0.3048  # Convert to m
+        length = wall.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble() * 0.3048 # Convert to m
+        area = wall.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsDouble() * 0.092903  # Convert to 
+
         walls.append({
-            "Width": wall.Width * 0.3048,  # Convert to m
-            "Length": get_wall_length(doc, wall) * 0.3048,  # Convert to m
+            "Width": width,
+            "Length": length,
+            "Height": area / length,
+            "Area": area,
+            "Volume": area * width,
         })
 
     for wall in walls:
-        print("Width: {} Length: {}".format(
-            wall["Width"], wall["Length"]))
+        print("Width: {}m".format(
+            wall["Width"]))
+        print("Length: {}m".format(wall["Length"]))
+        print("Area: {}m2".format(wall["Area"]))
+        print("Altura: {}m".format(wall["Height"]))
+        print("Volumen: {}m3".format(wall["Volume"]))
+
 except:
     SystemExit
