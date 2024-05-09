12403124460a3090fcb702a680fee5db510c2cad
Cristian Hernandez <129994548+icristianhernandez@users.noreply.github.com>
2024-05-08 20:31:04 -0400

    script formatted

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -6,39 +6,42 @@ doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
 selected_walls_data = []
-volume_COVENIN_bricks = 0.00211 # Volume of a standard brick in cubic meters
+volume_COVENIN_bricks = 0.00211  # Volume of a standard brick in cubic meters
+
 
 def get_wall_layer_info(wall):
-  """
-  This function retrieves information about the name and width of each layer in a wall.
+    """
+    This function retrieves information about the name and width of each layer in a wall.
 
-  Args:
-      wall: A Revit Wall element.
+    Args:
+        wall: A Revit Wall element.
 
-  Returns:
-      A dictionary where keys are material names and volume in feet.
-  """
-  # Check if the wall type has a compound structure (layered walls)
-  compound_structure = wall.WallType.GetCompoundStructure()
-  if not compound_structure:
-    return {}  # Wall has no layers
+    Returns:
+        A dictionary where keys are material names and volume in feet.
+    """
+    # Check if the wall type has a compound structure (layered walls)
+    compound_structure = wall.WallType.GetCompoundStructure()
+    if not compound_structure:
+        return {}  # Wall has no layers
 
-  layer_info = {}
-  for layer in compound_structure.GetLayers():
-    material = doc.GetElement(layer.MaterialId)
-    material_name = material.Name  # Assuming material has a name property
-    layer_width = layer.Width  # Volume in cubic meters
+    layer_info = {}
+    for layer in compound_structure.GetLayers():
+        material = doc.GetElement(layer.MaterialId)
+        material_name = material.Name  # Assuming material has a name property
+        layer_width = layer.Width  # Volume in cubic meters
 
-    layer_info[material_name] = {
-      'width': layer_width,
-    }
+        layer_info[material_name] = {
+            "width": layer_width,
+        }
 
-  return layer_info
+    return layer_info
+
 
 class SelectionFilter_Walls(ISelectionFilter):
     def AllowElement(self, element):
         if type(element) == Wall:
             return True
+
 
 Wall_filter = SelectionFilter_Walls()
 try:
@@ -48,15 +51,20 @@ try:
 
     for wall in picked_walls:
         width = wall.Width * 0.3048  # Convert to m
-        length = wall.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble() * 0.3048 # Convert to m
-        area = wall.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsDouble() * 0.092903  # Convert to 
-        name =  wall.Name
+        length = (
+            wall.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble() * 0.3048
+        )  # Convert to m
+        area = (
+            wall.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED).AsDouble()
+            * 0.092903
+        )  # Convert to
+        name = wall.Name
         layer_info = get_wall_layer_info(wall)
 
         if layer_info:
-          for material_name, data in layer_info.items():
-            if "Brick" in material_name:
-              bricks_layer_volume = data["width"] * area * 0.3048  # Convert to m3
+            for material_name, data in layer_info.items():
+                if "Brick" in material_name:
+                    bricks_layer_volume = data["width"] * area * 0.3048  # Convert to m3
 
         """
         The following data is calculated for each wall and added to the selected_walls_data list:
@@ -69,16 +77,22 @@ try:
         - bricks_layer_volume: The volume of the bricks layer in cubic meters. If the wall has no bricks layer, this value is 0.
         - necesary_bricks: The number of bricks needed to build the wall. Assuming the standard volumen given by COVENIN in cubic meters and adding 10% extra bricks for precaution.
         """
-        selected_walls_data.append({
-            "width": format(width, '.3f'),
-            "length": format(length, '.3f'),
-            "height": format(area / length, '.3f'),
-            "area": format(area, '.3f'),
-            "volume": format(area * width, '.3f'),
-            "name": name,
-            "bricks_layer_volume": format(bricks_layer_volume, '.3f') if bricks_layer_volume > 0 else 0,
-            "necesary_bricks": int(math.ceil(bricks_layer_volume / volume_COVENIN_bricks * 1.1)), # 10% extra bricks
-        })
+        selected_walls_data.append(
+            {
+                "width": format(width, ".3f"),
+                "length": format(length, ".3f"),
+                "height": format(area / length, ".3f"),
+                "area": format(area, ".3f"),
+                "volume": format(area * width, ".3f"),
+                "name": name,
+                "bricks_layer_volume": (
+                    format(bricks_layer_volume, ".3f") if bricks_layer_volume > 0 else 0
+                ),
+                "necesary_bricks": int(
+                    math.ceil(bricks_layer_volume / volume_COVENIN_bricks * 1.1)
+                ),  # 10% extra bricks
+            }
+        )
 
 except:
     SystemExit
