1c08714f1ff94b272d60a02a92a07ffbfbbc9e09
Cristian Hernandez <129994548+icristianhernandez@users.noreply.github.com>
2024-05-22 16:56:13 -0400

    better show covenin schedule name

--- covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Computos Metricos.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Computos Metricos.pushbutton/script.py
@@ -10,7 +10,7 @@ DOC = __revit__.ActiveUIDocument.Document
 UIDOC = __revit__.ActiveUIDocument
 
 
-def create_and_show_metric_schedule(selected_family_identifier):
+def create_and_show_covenin_calc_schedules(selected_family_identifier):
     UIDOC.ActiveView = create_metric_calc_schedule(DOC, selected_family_identifier)
 
 
@@ -22,19 +22,19 @@ class modalform(WPFWindow):
     def walls_push_button(self, sender, e):
         self.hide()
         user_selection_identifier = "Paredes"
-        create_and_show_metric_schedule(user_selection_identifier)
+        create_and_show_covenin_calc_schedules(user_selection_identifier)
         self.Close()
 
     def floors_push_button(self, sender, e):
         self.hide()
         user_selection_identifier = "Suelos"
-        create_and_show_metric_schedule(user_selection_identifier)
+        create_and_show_covenin_calc_schedules(user_selection_identifier)
         self.Close()
 
     def roofs_push_button(self, sender, e):
         self.hide()
         user_selection_identifier = "Techos"
-        create_and_show_metric_schedule(user_selection_identifier)
+        create_and_show_covenin_calc_schedules(user_selection_identifier)
         self.Close()
 
     def cancel_button(self, sender, e):
