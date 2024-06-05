b37a33aec137b2bf622355f3efe948144fde6983
Cristian Hernandez <129994548+icristianhernandez@users.noreply.github.com>
2024-06-05 18:58:14 -0400

    refactor: debugs tools and reimplemented add schedule fields
    created functions to add the fields to schedule
    changed the fields implementation way from Revit elements to Field Name
    updated covenin handler to new fields way
    updated docs
    add debug tools from get fields names & categories names

--- covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Computos Metricos.pushbutton/covenin_metric_calculation.py
+++ covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Computos Metricos.pushbutton/covenin_metric_calculation.py
@@ -3,29 +3,94 @@ from Autodesk.Revit.UI import *
 from Autodesk.Revit.UI.Selection import *
 from Autodesk.Revit.ApplicationServices import *
 
+def add_fields_to_schedule(doc, schedule, list_fields_name):
+    """
+    Add fields to a schedule
 
-def create_schedule(doc, category, schedule_fields):
+    Args:
+        doc (Document): The Revit document in which the schedule will be created.
+        schedule (ViewSchedule): The schedule to which the fields will be added.
+        list_fields_name (list of strings): A list of BuiltInParameter values representing the element parameters to be included in the schedule.
+
+    Returns:
+        ViewSchedule: The schedule with the added fields.
+
+    Examples:
+        1. add_fields_to_schedule(doc, schedule, ["Family and Type", "Base Constraint"])
+              This example adds fields for element family and type parameter and wall base constraint parameter to the schedule.
+    """
+    schedule_def = schedule.Definition
+    schedule_fields = schedule_def.GetSchedulableFields()
+
+    for desired_fields in list_fields_name:
+        for schedulable_fields in schedule_fields:
+            if schedulable_fields.GetName(doc) == desired_fields:
+                schedule_def.AddField(schedulable_fields)
+
+    return schedule
+
+def debug_get_schedulable_fields_names(doc, schedule_def):
+    """
+    Return a list with the names of the schedulable fields of a schedule
+
+    Args:
+        doc (Document): The Revit document in which the schedule will be created.
+        schedule (ViewSchedule): The schedule to which the fields will be added.
+
+    Returns:
+        schedule_fields_names (list of strings): A list of the names of the schedulable fields of the schedule.
+    """
+    schedule_fields = schedule_def.GetSchedulableFields()
+    schedule_fields_names = []
+
+    for schedulable_fields in schedule_fields:
+        schedule_fields_names.append(schedulable_fields.GetName(doc))
+
+    return schedule_fields_names
+
+def debug_get_all_categories(doc):
+    """
+    Return a list with the names of all the categories in the Revit document
+
+    Args:
+        doc (Document): The Revit document in which the schedule will be created.
+
+    Returns:
+        categories_names (list of strings): A list of the names of all the categories in the Revit document.
+    """
+    categories = doc.Settings.Categories
+    categories_names = []
+
+    for category in categories:
+        categories_names.append(category.Name)
+
+    return categories_names
+
+def print_string_list(string_list):
+    """
+    Print a list of strings in a readable format
+
+    Args:
+        string_list (list of strings): The list of strings to be printed.
+    """
+    for string in string_list:
+        print(string)
+
+def create_schedule(doc, category, schedule_show_fields):
     """
     Create a schedule of all the elements of a given category with fields of the desired element parameters
 
     Args:
         doc (Document): The Revit document in which the schedule will be created.
         category (BuiltInCategory): The category of the elements to be scheduled.
-        schedule_fields (list): A list of BuiltInParameter values representing the element parameters to be included in the schedule.
+        schedule_fields (list of strings): List of SchedulableField names to be included in the schedule.
 
     Returns:
         ViewSchedule: The created schedule with the desired fields.
 
     Examples:
-        1. create_schedule(doc, BuiltInCategory.OST_Walls, [BuiltInParameter.ELEM_FAMILY_AND_TYPE_PARAM, BuiltInParameter.WALL_BASE_CONSTRAINT])
+        1. create_schedule(doc, BuiltInCategory.OST_Walls, ["Family and Type", "Base Constraint"])  
            This example creates a schedule for walls with fields for element family and type parameter and wall base constraint parameter.
-
-        2.
-            with Transaction(doc, "Schedule") as schedule_transaction:
-                schedule_transaction.Start()
-                schedule = create_schedule(doc, BuiltInCategory.OST_Walls, [BuiltInParameter.ELEM_FAMILY_AND_TYPE_PARAM])
-                schedule_transaction.Commit()
-            __revit__.ActiveUIDocument.ActiveView = schedule
 
     Note:
         The schedule needs to be declared within a transaction in order to be properly instantiated in the Revit document.
@@ -36,15 +101,10 @@ def create_schedule(doc, category, schedule_fields):
 
     schedule = ViewSchedule.CreateSchedule(doc, all_id_of_category_elements)
     schedule_def = schedule.Definition
-    schedule_parameters_list = schedule_def.GetSchedulableFields()
 
-    # add only the desired fields to the schedule
-    for schedule_parameter_posibilities in schedule_parameters_list:
-        for desired_parameter in schedule_fields:
-            if schedule_parameter_posibilities.ParameterId == ElementId(
-                desired_parameter
-            ):
-                schedule_def.AddField(schedule_parameter_posibilities)
+    # print_string_list(debug_get_all_categories(doc))
+    # print_string_list(debug_get_schedulable_fields_names(doc, schedule_def)))
+    schedule = add_fields_to_schedule(doc, schedule, schedule_show_fields)
 
     return schedule
 
@@ -71,20 +131,20 @@ def create_metric_calc_schedule(doc, element_category):
         "Suelos"
 
     Notes: 
-        Needed be assigned to the active view in order to be displayed: uidoc.ActiveView = schedule
+        Needed be assigned to the active view in order to be displayed as tab: uidoc.ActiveView = schedule
     """
     covenin_metrics_of_elements = {
         "Paredes": {
             "revit_category": BuiltInCategory.OST_Walls,
-            "metrics": BuiltInParameter.HOST_AREA_COMPUTED,
+            "metrics": "Area"
         },
         "Techos": {
             "revit_category": BuiltInCategory.OST_Ceilings,
-            "metrics": BuiltInParameter.HOST_AREA_COMPUTED,
+            "metrics": "Area"
         },
         "Suelos": {
             "revit_category": BuiltInCategory.OST_Floors,
-            "metrics": BuiltInParameter.HOST_AREA_COMPUTED,
+            "metrics": "Area"
         },
     }
 
@@ -92,7 +152,7 @@ def create_metric_calc_schedule(doc, element_category):
 
     element_categorie = family_selected["revit_category"]
     desired_schedule_parameters = [
-        BuiltInParameter.ELEM_FAMILY_AND_TYPE_PARAM,
+        "Family and Type",
         family_selected["metrics"],
     ]
 
