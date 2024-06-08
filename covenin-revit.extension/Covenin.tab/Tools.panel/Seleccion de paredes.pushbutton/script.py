8121368bdf3e73a8c377f1340c8d6833699a5cfa
gusalviarez <gusalv2001@gmail.com>
2024-06-08 16:03:17 -0400

    print __revit__

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -4,10 +4,4 @@ from Autodesk.Revit.UI.Selection import *
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-def print_properties(self):
-    for prop, value in vars(self).items():
-        print(prop, ":", value) # or use format
-
-doc.print_properties()
-print("********************************")
-uidoc.print_properties()
+print(__revit__)
