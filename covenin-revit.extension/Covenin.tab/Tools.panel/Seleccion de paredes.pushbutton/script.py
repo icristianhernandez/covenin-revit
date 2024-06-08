4bf567d7af6e6ccbb81698c0d1f33dbd11accf19
gusalviarez <gusalv2001@gmail.com>
2024-06-08 16:35:08 -0400

    add ppretty

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -1,7 +1,12 @@
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI.Selection import *
+from ppretty import ppretty
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-print(__revit__)
+print(ppretty(__revit__))
+print("********************")
+print(ppretty(doc))
+print("********************")
+print(ppretty(uidoc))
