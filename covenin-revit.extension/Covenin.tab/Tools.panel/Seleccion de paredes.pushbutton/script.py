a963c762288d7c847c3c093e291efde12c1af55a
gusalviarez <gusalv2001@gmail.com>
2024-06-08 15:26:08 -0400

    create type v1

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -3,3 +3,8 @@ from Autodesk.Revit.UI.Selection import *
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
+# create a new wall family type
+wallType = WallType.Create(doc, "MyWallType")
+
+print("Wall type created: ", wallType.Name)
