d30e201d4ceef3a72980b1b586e81a917865624b
Cristian Hernandez <129994548+icristianhernandez@users.noreply.github.com>
2024-06-07 22:38:12 -0400

    implement sorting config to covenin schedules
    - Implemeted the sorting to create schedules
    - Implemented a function to get the fields of a schedules
    - Implemented the sorting optiosn to covenin schedules

--- covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Computos Metricos.pushbutton/covenin_metric_calculation.py
+++ covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Computos Metricos.pushbutton/covenin_metric_calculation.py
@@ -3,6 +3,7 @@ from Autodesk.Revit.UI import *
 from Autodesk.Revit.UI.Selection import *
 from Autodesk.Revit.ApplicationServices import *
 
+
 def add_fields_to_schedule(doc, schedule, list_fields_name):
     """
     Add fields to a schedule
@@ -29,6 +30,7 @@ def add_fields_to_schedule(doc, schedule, list_fields_name):
 
     return schedule
 
+
 def debug_get_schedulable_fields_names(doc, schedule_def):
     """
     Return a list with the names of the schedulable fields of a schedule
@@ -48,6 +50,7 @@ def debug_get_schedulable_fields_names(doc, schedule_def):
 
     return schedule_fields_names
 
+
 def debug_get_all_categories(doc):
     """
     Return a list with the names of all the categories in the Revit document
@@ -66,6 +69,7 @@ def debug_get_all_categories(doc):
 
     return categories_names
 
+
 def print_list_OfStrings(string_list):
     """
     Print a list of strings in a readable format
@@ -76,7 +80,70 @@ def print_list_OfStrings(string_list):
     for string in string_list:
         print(string)
 
-def create_schedule(doc, category, schedule_show_fields):
+
+def get_schedule_fields_with_ids(doc, schedule):
+    """
+    Return a dictionary with the fields name as key and fields id as value of a
+    schedule
+
+    Args:
+        doc (Document): The Revit document in which the schedule will be created.
+        schedule (ViewSchedule): The schedule whose fields will be returned.
+
+    Returns:
+        fields_with_ids (dict): A dictionary with the fields name as key and fields id as value of the schedule.
+    """
+    schedule_def = schedule.Definition
+    fields_with_ids = {}
+
+    for i in range(schedule_def.GetFieldCount()):
+        field = schedule_def.GetField(i)
+        fields_with_ids[field.GetName()] = field.FieldId
+
+    return fields_with_ids
+
+
+def add_schedule_sorting_field(doc, schedule, sort_settings):
+    """
+    Add an sorting field with settings to a schedule
+
+    Args:
+        doc (Document): The Revit document in which the schedule will be created.
+        schedule (ViewSchedule): The schedule to which the sorting field will be added.
+        sort_settings (dict): A dictionary with the settings of the sorting field.
+
+    Returns:
+        ViewSchedule: The schedule with the added sorting field.
+
+    Examples:
+        1. add_schedule_sorting_field(doc, schedule, {"field": "Family and Type", "order": ScheduleSortOrder.Ascending})
+              This example adds a sorting field for element family and type parameter in ascending order to the schedule.
+    """
+    schedule_def = schedule.Definition
+    schedule_fields_and_ids = get_schedule_fields_with_ids(doc, schedule)
+    sort_group = ScheduleSortGroupField()
+
+    if "field" in sort_settings:
+        sort_group.FieldId = schedule_fields_and_ids[sort_settings["field"]]
+    if "show_blank_line" in sort_settings:
+        sort_group.ShowBlankLine = sort_settings["show_blank_line"]
+    if "show_footer" in sort_settings:
+        sort_group.ShowFooter = sort_settings["show_footer"]
+    if "show_footer_count" in sort_settings:
+        sort_group.ShowFooterCount = sort_settings["show_footer_count"]
+    if "show_footer_title" in sort_settings:
+        sort_group.ShowFooterTitle = sort_settings["show_footer_title"]
+    if "show_header" in sort_settings:
+        sort_group.ShowHeader = sort_settings["show_header"]
+    if "sort_order" in sort_settings:
+        sort_group.SortOrder = sort_settings["sort_order"]
+
+    schedule_def.AddSortGroupField(sort_group)
+
+    return schedule
+
+
+def create_schedule(doc, category, schedule_show_fields, list_of_sort_settings=None):
     """
     Create a schedule of all the elements of a given category with fields of the desired element parameters
 
@@ -89,7 +156,7 @@ def create_schedule(doc, category, schedule_show_fields):
         ViewSchedule: The created schedule with the desired fields.
 
     Examples:
-        1. create_schedule(doc, BuiltInCategory.OST_Walls, ["Family and Type", "Base Constraint"])  
+        1. create_schedule(doc, BuiltInCategory.OST_Walls, ["Family and Type", "Base Constraint"])
            This example creates a schedule for walls with fields for element family and type parameter and wall base constraint parameter.
 
     Note:
@@ -100,11 +167,14 @@ def create_schedule(doc, category, schedule_show_fields):
     with Transaction(doc, "Schedule") as t:
         t.Start()
 
-        all_id_of_category_elements = ElementId(category)
-        schedule = ViewSchedule.CreateSchedule(doc, all_id_of_category_elements)
+        category_id = ElementId(category)
+        schedule = ViewSchedule.CreateSchedule(doc, category_id)
         schedule_def = schedule.Definition
 
         schedule = add_fields_to_schedule(doc, schedule, schedule_show_fields)
+        if list_of_sort_settings:
+            for sort_setting in list_of_sort_settings:
+                schedule = add_schedule_sorting_field(doc, schedule, sort_setting)
 
         t.Commit()
 
@@ -145,73 +215,49 @@ def create_metric_calc_schedule(doc, element_category):
         "Ventanas"
         "Muros Cortina"
 
-    Notes: 
+    Notes:
         Needed be assigned to the active view in order to be displayed as tab: uidoc.ActiveView = schedule
     """
     covenin_metrics_of_elements = {
-        "Paredes": {
-            "revit_category": BuiltInCategory.OST_Walls,
-            "metrics": "Area"
-        },
-        "Techos": {
-            "revit_category": BuiltInCategory.OST_Ceilings,
-            "metrics": "Area"
-        },
-        "Suelos": {
-            "revit_category": BuiltInCategory.OST_Floors,
-            "metrics": "Area"
-        },
+        "Paredes": {"revit_category": BuiltInCategory.OST_Walls, "metrics": "Area"},
+        "Techos": {"revit_category": BuiltInCategory.OST_Ceilings, "metrics": "Area"},
+        "Suelos": {"revit_category": BuiltInCategory.OST_Floors, "metrics": "Area"},
         "Columnas": {
             "revit_category": BuiltInCategory.OST_Columns,
-            "metrics": "Volume"
+            "metrics": "Volume",
         },
-        "Puertas": {
-            "revit_category": BuiltInCategory.OST_Doors,
-            "metrics": "Count"
-        },
+        "Puertas": {"revit_category": BuiltInCategory.OST_Doors, "metrics": "Count"},
         "Bordas de Losa": {
             "revit_category": BuiltInCategory.OST_EdgeSlab,
-            "metrics": "Length"
+            "metrics": "Length",
         },
         "Mobiliario": {
             "revit_category": BuiltInCategory.OST_Furniture,
-            "metrics": "Count"
+            "metrics": "Count",
         },
         "Sistemas de Mobiliario": {
             "revit_category": BuiltInCategory.OST_FurnitureSystems,
-            "metrics": "Count"
+            "metrics": "Count",
         },
         "Iluminacion": {
             "revit_category": BuiltInCategory.OST_LightingFixtures,
-            "metrics": "Count"
+            "metrics": "Count",
         },
         "Dispositivos de Iluminacion": {
             "revit_category": BuiltInCategory.OST_LightingDevices,
-            "metrics": "Count"
+            "metrics": "Count",
         },
-        "Plantas": {
-            "revit_category": BuiltInCategory.OST_Planting,
-            "metrics": "Count"
-        },
-        "Cubiertas": {
-            "revit_category": BuiltInCategory.OST_Roofs,
-            "metrics": "Area"
-        },
-        "Escaleras": {
-            "revit_category": BuiltInCategory.OST_Stairs,
-            "metrics": "Area"
-        },
+        "Plantas": {"revit_category": BuiltInCategory.OST_Planting, "metrics": "Count"},
+        "Cubiertas": {"revit_category": BuiltInCategory.OST_Roofs, "metrics": "Area"},
+        "Escaleras": {"revit_category": BuiltInCategory.OST_Stairs, "metrics": "Area"},
         "Estructuras Temporales": {
             "revit_category": BuiltInCategory.OST_TemporaryStructure,
-            "metrics": "Volume"
+            "metrics": "Volume",
         },
-        "Ventanas": {
-            "revit_category": BuiltInCategory.OST_Windows,
-            "metrics": "Count"
-        },
+        "Ventanas": {"revit_category": BuiltInCategory.OST_Windows, "metrics": "Count"},
         "Muros Cortina": {
             "revit_category": BuiltInCategory.OST_CurtainWallPanels,
-            "metrics": "Area"
+            "metrics": "Area",
         },
     }
 
@@ -221,8 +267,28 @@ def create_metric_calc_schedule(doc, element_category):
     desired_schedule_parameters = [
         "Family and Type",
         family_selected["metrics"],
+        "Count",
+    ]
+    list_of_sort_settings = [
+        {
+            "field": "Family and Type",
+            "show_blank_line": True,
+            "sort_order": ScheduleSortOrder.Ascending,
+            "show_footer": True,
+            "show_footer_count": True,
+            "show_header": True,
+        },
+        {
+            "field": family_selected["metrics"],
+            "sort_order": ScheduleSortOrder.Descending,
+        },
     ]
 
-    schedule = create_schedule(doc, element_categorie_revitID, desired_schedule_parameters)
+    schedule = create_schedule(
+        doc,
+        element_categorie_revitID,
+        desired_schedule_parameters,
+        list_of_sort_settings,
+    )
 
     return schedule
