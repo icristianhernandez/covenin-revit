3cc32b55d9d3679f2c117beffca4850f5addd093
gusalviarez <gusalv2001@gmail.com>
2024-06-09 14:56:19 -0400

    delete transaction

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -5,13 +5,9 @@ doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 fec = FilteredElementCollector(doc)
 
-t = Transaction(doc, 'Create family instance.')
-
-t.Start()
-
 all_families = fec.OfClass(FamilySymbol).ToElements()
 print('----------------------------')
 
 for item in all_families:
     print(item.Name)
-t.Commit()
+
