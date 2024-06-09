8886193bfb4600eb409fd12fa0961d46a038f468
gusalviarez <gusalv2001@gmail.com>
2024-06-09 13:18:30 -0400

    print all walls

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -4,9 +4,10 @@ import pprint
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+fec = FilteredElementCollector(doc)
 
-pprint.pprint(__revit__)
-print("********************")
-pprint.pprint(doc)
-print("********************")
-pprint.pprint(uidoc)
+
+all_walls = fec.OfClass(Wall).ToElements()
+print(all_walls)
+print('----------------------------')
+print(fec.OfClass(Wall)) 
