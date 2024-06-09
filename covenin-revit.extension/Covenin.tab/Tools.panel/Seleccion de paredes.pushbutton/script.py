085b22bf2707e936e5b1e7efaaa34a1e0be8f662
gusalviarez <gusalv2001@gmail.com>
2024-06-09 15:43:01 -0400

    set families

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -6,6 +6,7 @@ uidoc = __revit__.ActiveUIDocument
 fec = FilteredElementCollector(doc)
 
 all_families = fec.OfClass(FamilySymbol).ToElements()
+all_families = list(set(all_families))
 print('----------------------------')
 
 for item in all_families:
