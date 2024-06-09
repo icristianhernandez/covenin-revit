efb571f514b3c50185ee480b47a169b7b7e72825
gusalviarez <gusalv2001@gmail.com>
2024-06-09 13:27:07 -0400

    print all walltypes and walls

--- covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
+++ covenin-revit.extension/Covenin.tab/Tools.panel/Seleccion de paredes.pushbutton/script.py
@@ -1,13 +1,13 @@
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI.Selection import *
-import pprint
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 fec = FilteredElementCollector(doc)
 
 
-all_walls = fec.OfClass(Wall).WhereElementIsElementType().ToElements()
+all_walls_types = fec.OfClass(Wall).WhereElementIsElementType().ToElements()
+all_walls = fec.OfClass(Wall).WhereElementIsNotElementType().ToElements()
 print(all_walls)
 print('----------------------------')
-print(fec.OfClass(Wall))
+print(all_walls_types)
