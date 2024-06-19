6c56ce66d54d0570b91fffae0b1a2f84f01f0df0
Migue-709 <orozcoml709@gmail.com>
2024-06-19 17:55:04 -0400

    Updated export. Title formats, borders, cell sizes, etc

--- covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Exportar y Unificar.pushbutton/export_schedules.py
+++ covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Exportar y Unificar.pushbutton/export_schedules.py
@@ -1,4 +1,5 @@
-from pyrevit import DB, forms
+# -*- coding: utf-8 -*-
+from pyrevit import revit, DB, forms, script
 from Autodesk.Revit.UI import *
 from Autodesk.Revit.DB import *
 import xlsxwriter
@@ -8,8 +9,21 @@ def export_selected_schedules(selected_schedules):
     path = forms.save_file(file_ext="xlsx")
     workbook = xlsxwriter.Workbook(path)
     worksheet = workbook.add_worksheet()
+    sheet_name = "Tabla de Planificación Unificada"
+    title_format = workbook.add_format(
+        {
+            "bold": True,
+            "font_size": 12,
+            "align": "center",
+            "bg_color": "#D3D3D3",
+            "border": 1,
+        }
+    )
+    borders_format = workbook.add_format({"border": 1, "align": "left"})
+    worksheet.set_column("B:B", 32)
     schedules_data = []
     space_between_schedules = " "
+
     for scheds in selected_schedules:
         table_data = scheds.GetTableData()
         section_data = table_data.GetSectionData(DB.SectionType.Body)
@@ -26,7 +40,18 @@ def export_selected_schedules(selected_schedules):
             schedules_data.append(rows)
         schedules_data.append(space_between_schedules)
 
-        for row_num, row_data in enumerate(schedules_data):
-            for column_num, column_data in enumerate(row_data):
-                worksheet.write(row_num, column_num, column_data)
+    worksheet.merge_range("B1:C1", sheet_name, title_format)
+
+    row = 1
+
+    for schedule_rows in schedules_data:
+        col = 1
+        for data in schedule_rows:
+            if data == " ":
+                worksheet.write(row, col, data)
+            else:
+                worksheet.write(row, col, data, borders_format)
+                col += 1
+        row += 1
+
     workbook.close()
