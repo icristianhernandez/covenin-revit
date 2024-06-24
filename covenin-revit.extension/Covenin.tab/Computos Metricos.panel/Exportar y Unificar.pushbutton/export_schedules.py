0686eed96d02883e7ce665c629c62320d9dda622
Migue-709 <orozcoml709@gmail.com>
2024-06-24 15:17:09 -0400

    Added calculation of metric computation subtotals in export to Excel

--- covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Exportar y Unificar.pushbutton/export_schedules.py
+++ covenin-revit.extension/Covenin.tab/Computos Metricos.panel/Exportar y Unificar.pushbutton/export_schedules.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
-from pyrevit import revit, DB, forms, script
-from Autodesk.Revit.UI import *
-from Autodesk.Revit.DB import *
+from pyrevit import DB, forms
 import xlsxwriter
+
+
+def check_subtotal_unit(field):
+    if field[0] == "Area" or field[0] == "Área":
+        unit = "m²"
+    elif field[0] == "Volume" or field[0] == "Volumen":
+        unit = "m³"
+
+    else:
+        unit = ""
+    return unit
+
+
+def calculate_subtotal(column):
+    new_column = []
+
+    for item in column:
+        if item:
+            try:
+                new_data = int(item.split()[0])
+                new_column.append(new_data)
+            except ValueError:
+                pass
+
+    sum_of_values = sum(new_column)
+    subtotal = str(sum_of_values) + " "
+    return subtotal
 
 
 def export_selected_schedules(selected_schedules):
@@ -26,6 +51,8 @@ def export_selected_schedules(selected_schedules):
         space_between_schedules = " "
 
         for scheds in selected_schedules:
+            temporal_data = []
+            subtotal_column = []
             table_data = scheds.GetTableData()
             section_data = table_data.GetSectionData(DB.SectionType.Body)
             number_of_rows = section_data.NumberOfRows
@@ -38,8 +65,22 @@ def export_selected_schedules(selected_schedules):
                         DB.SectionType.Body, rows_number, columns_number
                     )
                     rows.append(cells_content)
+
+                temporal_data.append(rows)
                 schedules_data.append(rows)
+
+            for _, second_column in temporal_data:
+                subtotal_column.append(second_column)
+
+            subtotal = calculate_subtotal(subtotal_column)
+
+            unit = check_subtotal_unit(subtotal_column)
+
+            schedules_data.append(["SubTotal: ", subtotal + unit])
             schedules_data.append(space_between_schedules)
+
+            del temporal_data[:]
+            del subtotal_column[:]
 
         worksheet.merge_range("B1:C1", sheet_name, title_format)
 
@@ -47,15 +88,14 @@ def export_selected_schedules(selected_schedules):
 
         for schedule_rows in schedules_data:
             col = 1
-            for data in schedule_rows:
-                if data == " ":
-                    worksheet.write(row, col, data)
+            for cell_data in schedule_rows:
+                if cell_data == " ":
+                    worksheet.write(row, col, cell_data)
                 else:
-                    worksheet.write(row, col, data, borders_format)
+                    worksheet.write(row, col, cell_data, borders_format)
                     col += 1
             row += 1
 
         workbook.close()
-
     except:
         SystemExit
